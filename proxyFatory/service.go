c486c5b47741ba63d93b7657499d66fd911aad07
abser <abserari@gmail.com>
2020-03-13 00:45:31 +0800

    update some ideas

---
+++ proxyFatory/service.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+)
+
+const (
+	defaultHealthCheckInterval = 30 * time.Second
+	defaultHealthCheckTimeout  = 5 * time.Second
+)
+
+const defaultMaxBodySize int64 = -1
+
+// ServiceManager according to service generate handler to handle request.
+type ServiceManager struct {
+	ServiceConfig
+}
+
+type ServiceConfig struct {
+	ResponseForwardingConfig
+}
+
+type ResourceService struct {
+	
+}
